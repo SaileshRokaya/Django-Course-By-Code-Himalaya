cbac0176680411d310d4e36cbbb15c9220f5882d
SaileshRokaya <79441756+SaileshRokaya@users.noreply.github.com>
2022-08-02 09:50:07 +0545

    Convert a matrix

--- BasicPython/Assignment/02_Convert_a_matrix.py
+++ BasicPython/Assignment/02_Convert_a_matrix.py
@@ -1,1 +1,10 @@
 # Q.No 2) Convert a matrix : [[1,2], [3,4], [5,6],[7,8]] into [[1,3,5,7][2,4,6,8]]
+
+def matrix():
+ matrix1 = [[1,2], [3,4], [5,6],[7,8]]
+ 
+ for row in matrix1:
+  print(row)
+  for i in range(2):
+    print(row[i])
+matrix()
